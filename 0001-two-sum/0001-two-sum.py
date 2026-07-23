986a913335963086e887738d62d46030608252fa
dahiyakansha04-ctrl <dahiyakansha04@gmail.com>
2026-07-23 10:27:25 +0530

    Time: 2 ms (57.39%), Space: 13 MB (66.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,10 @@
 class Solution:
     def twoSum(self, nums, target):
-        prevMap={}
+        prevMap = {}
 
         for i,n in enumerate(nums):
-            diff=target-n
+            diff= target-n
             if diff in prevMap:
                 return [prevMap[diff],i]
             prevMap[n]=i
+       
